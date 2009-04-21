2aa909988e03da1dea7d9b575b9e3ea2ace2476b
Erica Sadun <erica@ericasadun.com>
2009-04-20 20:09:48 -0600

    Trying to rename Hardware

---
+++ UIDevice-Hardware.m
@@ -0,0 +1,171 @@
+/*
+ Erica Sadun, http://ericasadun.com
+ iPhone Developer's Cookbook, 3.0 Edition
+ BSD License, Use at your own risk
+ */
+
+//  Thanks to Emanuele Vulcano, Kevin Ballard/Eridius, Ryandjohnson, Matt Brown, etc.
+// TTD:  - Bluetooth?  Screen pixels? Dot pitch? Accelerometer? GPS enabled/disabled
+
+#include <sys/sysctl.h>
+#include <net/if.h>
+#include <net/if_dl.h>
+#import "UIDevice-Hardware.h"
+
+@implementation UIDevice (Hardware)
+
+/*
+ Platforms
+ iPhone1,1 -> iPhone 1G
+ iPhone1,2 -> iPhone 3G 
+ iPod1,1   -> iPod touch 1G 
+ iPod2,1   -> iPod touch 2G 
+*/
+
+
+#pragma mark sysctlbyname utils
+- (NSString *) getSysInfoByName:(char *)typeSpecifier
+{
+	size_t size;
+    sysctlbyname(typeSpecifier, NULL, &size, NULL, 0);
+    char *answer = malloc(size);
+	sysctlbyname(typeSpecifier, answer, &size, NULL, 0);
+	NSString *results = [NSString stringWithCString:answer encoding: NSUTF8StringEncoding];
+	free(answer);
+	return results;
+}
+
+- (NSString *) platform
+{
+	return [self getSysInfoByName:"hw.machine"];
+}
+
+#pragma mark sysctl utils
+- (NSUInteger) getSysInfo: (uint) typeSpecifier
+{
+	size_t size = sizeof(int);
+	int results;
+	int mib[2] = {CTL_HW, typeSpecifier};
+	sysctl(mib, 2, &results, &size, NULL, 0);
+	return (NSUInteger) results;
+}
+
+- (NSUInteger) cpuFrequency
+{
+	return [self getSysInfo:HW_CPU_FREQ];
+}
+
+- (NSUInteger) busFrequency
+{
+	return [self getSysInfo:HW_BUS_FREQ];
+}
+
+- (NSUInteger) totalMemory
+{
+	return [self getSysInfo:HW_PHYSMEM];
+}
+
+- (NSUInteger) userMemory
+{
+	return [self getSysInfo:HW_USERMEM];
+}
+
+- (NSUInteger) maxSocketBufferSize
+{
+	return [self getSysInfo:KIPC_MAXSOCKBUF];
+}
+
+#pragma mark platform type and name utils
+- (NSUInteger) platformType
+{
+	NSString *platform = [self platform];
+	if ([platform isEqualToString:@"iPhone1,1"]) return UIDevice1GiPhone;
+	if ([platform isEqualToString:@"iPhone1,2"]) return UIDevice3GiPhone;
+	if ([platform isEqualToString:@"iPod1,1"])   return UIDevice1GiPod;
+	if ([platform isEqualToString:@"iPod2,1"])   return UIDevice2GiPod;
+	if ([platform hasPrefix:@"iPhone"]) return UIDeviceUnknowniPhone;
+	if ([platform hasPrefix:@"iPod"]) return UIDeviceUnknowniPod;
+	return UIDeviceUnknown;
+}
+
+- (NSString *) platformString
+{
+	switch ([self platformType])
+	{
+		case UIDevice1GiPhone: return IPHONE_1G_NAMESTRING;
+		case UIDevice3GiPhone: return IPHONE_3G_NAMESTRING;
+		case UIDeviceUnknowniPhone: return IPHONE_UNKNOWN_NAMESTRING;
+		
+		case UIDevice1GiPod: return IPOD_1G_NAMESTRING;
+		case UIDevice2GiPod: return IPOD_2G_NAMESTRING;
+		case UIDeviceUnknowniPod: return IPOD_UNKNOWN_NAMESTRING;
+
+		default: return nil;
+	}
+}
+
+#pragma mark  platform capabilities
+- (NSUInteger) platformCapabilities
+{
+	switch ([self platformType])
+	{
+		case UIDevice1GiPhone: return UIDeviceBuiltInSpeaker | UIDeviceBuiltInCamera | UIDeviceBuiltInMicrophone | UIDeviceSupportsExternalMicrophone | UIDeviceSupportsTelephony | UIDeviceSupportsVibration | UIDeviceBuiltInProximitySensor;
+		case UIDevice3GiPhone: return UIDeviceSupportsGPS | UIDeviceBuiltInSpeaker | UIDeviceBuiltInCamera | UIDeviceBuiltInMicrophone | UIDeviceSupportsExternalMicrophone | UIDeviceSupportsTelephony | UIDeviceSupportsVibration | UIDeviceBuiltInProximitySensor;
+		case UIDeviceUnknowniPhone: return UIDeviceBuiltInSpeaker | UIDeviceBuiltInCamera | UIDeviceBuiltInMicrophone | UIDeviceSupportsExternalMicrophone | UIDeviceSupportsTelephony | UIDeviceSupportsVibration | UIDeviceBuiltInProximitySensor;
+
+		case UIDevice1GiPod: return 0;
+		case UIDevice2GiPod: return UIDeviceBuiltInSpeaker | UIDeviceBuiltInMicrophone | UIDeviceSupportsExternalMicrophone;
+		case UIDeviceUnknowniPod: return 0;
+		
+		default: return 0;
+	}
+}
+
+#pragma mark MAC addy
+// Return the local MAC addy
+// Courtesy of FreeBSD hackers email list
+- (NSString *) macaddress
+{
+	int					mib[6];
+	size_t				len;
+	char				*buf;
+	unsigned char		*ptr;
+	struct if_msghdr	*ifm;
+	struct sockaddr_dl	*sdl;
+	
+	mib[0] = CTL_NET;
+	mib[1] = AF_ROUTE;
+	mib[2] = 0;
+	mib[3] = AF_LINK;
+	mib[4] = NET_RT_IFLIST;
+	
+	if ((mib[5] = if_nametoindex("en0")) == 0) {
+		printf("Error: if_nametoindex error\n");
+		return NULL;
+	}
+	
+	if (sysctl(mib, 6, NULL, &len, NULL, 0) < 0) {
+		printf("Error: sysctl, take 1\n");
+		return NULL;
+	}
+	
+	if ((buf = malloc(len)) == NULL) {
+		printf("Could not allocate memory. error!\n");
+		return NULL;
+	}
+	
+	if (sysctl(mib, 6, buf, &len, NULL, 0) < 0) {
+		printf("Error: sysctl, take 2");
+		return NULL;
+	}
+	
+	ifm = (struct if_msghdr *)buf;
+	sdl = (struct sockaddr_dl *)(ifm + 1);
+	ptr = (unsigned char *)LLADDR(sdl);
+	NSString *outstring = [NSString stringWithFormat:@"%02x:%02x:%02x:%02x:%02x:%02x", *ptr, *(ptr+1), *(ptr+2), *(ptr+3), *(ptr+4), *(ptr+5)];
+	free(buf);
+	return [outstring uppercaseString];
+}
+
+
+@end
